f1e165e4b3863932b4c0976f25fe67985a74f39c
habib4445 <0124habibur@gmail.com>
2025-10-25 18:07:57 +0600

    add

---
+++ agian.js
@@ -0,0 +1,10 @@
+const salary = 25000;
+const isBCS = 25000;
+const height = 6.6;
+
+if(salary > 20000 && height >70){
+    console.log('good')
+}
+else{
+    console.log('bad')
+}
---
+++ condition.js
@@ -0,0 +1,18 @@
+// var weight = 40;
+// if(weight > 20){
+//     console.log('i will buy');
+
+// }
+// else{
+//     console.log('i will not buy');
+// }
+const salary = 25000;
+const isBCS = 25000;
+const height = 6.6;
+
+if(salary > 20000 && height <70){
+    console.log('good')
+}
+else{
+    console.log('bad')
+}
